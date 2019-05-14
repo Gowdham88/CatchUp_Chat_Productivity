b181967312ffd4634e64846c4479628bfa6d7cb4
Siva <sivansn10@gmail.com>
2019-05-14 16:59:48 +0530

    login design and firebase pod,etc

---
+++ Catch Up/Controllers/CreateProfileController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateProfileController.swift
+//  Catch Up
+//
+//  Created by CZ Ltd on 5/14/19.
+//  Copyright © 2019 CZ Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CreateProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
